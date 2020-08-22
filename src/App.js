5897468de9ee21d668e5c62e68765f3ad45834fb
ShanelDavis <nshanelh@gmail.com>
2020-08-22 09:37:20 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const App = () =>{
-
+const App = () => {
   return (
     <div className="App">
-       <Header></Header>
-        <p>Body Here</p>
+      <Header></Header>
+      <p>Body Here</p>
       <Footer></Footer>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      ></a>
     </div>
   );
-}
+};
 
 export default App;
